b14d1d713505866b6639c879f121af4de086698c
Manan-200 <manandeopurkar@gmail.com>
2022-02-06 23:55:00 +0530

    Added code

--- Tests/Graph.py
+++ Tests/Graph.py
@@ -1,15 +1,19 @@
+import matplotlib.pyplot as plt
+
 a = []
 b = []
-import matplotlib.pyplot as plt
+
 for x in range (-1000,1001):
     for y in range (-1000,1001):
-        LHS = x 
-        RHS = 10
+        LHS = x**2
+        RHS = y
         if LHS == RHS:
             a.append(x)
             b.append(y)
+
 print ("x:",a)
 print ("y:",b)
+
 plt.plot(a, b)
 plt.xlabel('x-axis')
 plt.ylabel('y-axis')
--- Tests/memory eater.py
+++ Tests/memory eater.py
@@ -1,10 +1,6 @@
 a = 0
-b = 1
-c = 0
 list1 = []
-while a != b:
-    b += 1
-    a += 1
-    c += a ** 1000000000
+while True:
+    a += a ** 1000000000
     for n in range (1,1000000000000000000000000000000000):
-        list1.append(c)
+        list1.append(a)
