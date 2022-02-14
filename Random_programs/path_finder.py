45c4fb20e863ecae100fa1fdd6513fd60e71bec9
Manan-200 <manandeopurkar@gmail.com>
2022-02-14 17:51:25 +0530

    Added code

--- Random_programs/path_finder.py
+++ Random_programs/path_finder.py
@@ -1,14 +1,17 @@
 import pygame
 import random
 
+from setuptools_scm import NonNormalizedVersion
+
 pygame.font.init()
 
 #Variables
-nodes, combination_list, node_combination, distance_list = [], [], [], []
+nodes, nodes2, combination_list, node_combination, distance_list = [], [], [], [], []
 running = True
 height, width = 600, 800
 dist = 0
-counter_1, counter_2 = 1, 0
+min_display_dist, max_display_dist = 0, 0
+counter_1 = 1
 fps = 60
 counter = 0
 
@@ -29,7 +32,7 @@ class Node:
         self.x = random.randint(10, width - 10)
         self.y = random.randint(10, height - 10)
     def draw(self):
-        pygame.draw.rect(win, (0, 0, 255), (self.x - 10, self.y - 10, 10, 10))
+        pygame.draw.rect(win, (0, 0, 255), (self.x - 5, self.y - 5, 10, 10))
 
 #Creating nodes
 for i in range (5):
@@ -58,39 +61,55 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            print(dist)
 
     counter += 1
 
     win.fill((0, 0, 0))
-            
-    #Making node_combination
-    num1 = combination_list[counter_1 - 1]
-    for num2 in num1:
-        for node in nodes:
-            if node.id == num2 and len(node_combination) != 5:
-                node_combination.append(node)
-
-    #Clearing node_combination after every 5 steps
-    if len(node_combination) == 5 and counter%5 == 0:
-        counter_1 += 1
-        node_combination.clear()
-
-    #Calculating distance
-    for i in range(len(node_combination)):
-        if i < 4:
-            dist += distance(node_combination[i], node_combination[i+1])
-        elif i == 4:
-            dist += distance(node_combination[i], node_combination[0])
-    distance_list.append(dist)
-    dist = min(distance_list)
 
     #Drawing nodes
     for node in nodes:
         node.draw()
+            
+    #Making node_combination
+    var1 = combination_list[counter_1 - 1]
+    for var2 in var1:
+        for node in nodes:
+            if node.id == var2 and len(node_combination) != 5:
+                node_combination.append(node)
+    nodes2.append(node_combination)
+
+    #Clearing node_combination after every 5 steps
+    if counter % 2 == 0:
+        
+        if counter <= 120:
+            counter_1 += 1
+
+        for i in range(len(node_combination)):
+            if i < 4:
+                dist += distance(node_combination[i], node_combination[i+1])
+                pygame.draw.line(win, (255, 255, 255), (node_combination[i].x, node_combination[i].y), (node_combination[i+1].x, node_combination[i+1].y), 1)
+            elif i == 4:
+                dist += distance(node_combination[i], node_combination[0])
+                pygame.draw.line(win, (255, 255, 255), (node_combination[i].x, node_combination[i].y), (node_combination[0].x, node_combination[0].y), 1)
+        distance_list.append(dist)
+        dist = 0
+
+        max_display_dist = max(distance_list)
+        min_display_dist = min(distance_list)
+
+        node_combination.clear()
+
+        """var3 = nodes2[distance_list.index(min_display_dist)]
+        for i in range(len(var3)):
+            if i < 4:
+                pygame.draw.line(win, (255, 255, 255), (var3[i].x, var3[i].y), (var3[i+1].x, var3[i+1].y), 1)
+            elif i == 4:
+                pygame.draw.line(win, (255, 255, 255), (var3[i].x, var3[i].y), (var3[0].x, var3[0].y), 1)"""
 
     #Displaying distance
-    dist_text = dist_font.render(f"Distance : {round(dist)}", 1, (255, 255, 255))
-    win.blit(dist_text, (width - dist_text.get_width() - 10, 10))
+    min_dist_text = dist_font.render(f"Minimum distance : {round(min_display_dist)}", 1, (255, 255, 255))
+    max_dist_text = dist_font.render(f"Maximum distance : {round(max_display_dist)}", 1, (255, 255, 255))
+    win.blit(min_dist_text, (width - min_dist_text.get_width() - 10, 10))
+    win.blit(max_dist_text, (width - max_dist_text.get_width() - 10, 40))
 
     pygame.display.update()
