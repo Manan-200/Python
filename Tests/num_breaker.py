f65a2c212630a602616a8dde101809bbbd8784ea
Manan-200 <manandeopurkar@gmail.com>
2022-02-15 21:49:34 +0530

    Added code

--- Tests/num_breaker.py
+++ Tests/num_breaker.py
@@ -1,10 +1,10 @@
-remainder_list = []
+reversed_list = []
 
 num = int(input("Enter a number to break it into digits: "))
 
 while num != 0:
     r = num % 10
-    remainder_list.append(r)
+    reversed_list.append(r)
     num = num // 10
 
-print(remainder_list[::-1])
+print(reversed_list[::-1])
