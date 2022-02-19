c64c3f7ea2007043a1421e2b3c17a011d42acd29
Manan-200 <manandeopurkar@gmail.com>
2022-02-19 23:21:10 +0530

    Added code

--- Tests/self_explanatory_num.py
+++ Tests/self_explanatory_num.py
@@ -1,5 +1,20 @@
-a_list, b_list, c_list, d_list, e_list = [], [], [], [], []
-for i in range(1000):
+t1_list, t2_list, t3_list, t4_list = [], [], [], []
+t1, t2, t3, t4 = None, None, None, None
+
+def mult(l):
+    var = 0
+    for i in range(len(l)):
+        if i + 1 < len(l):
+            var += l[i] * l[i+1]
+    return var
+
+def check_valid(l):
+    for i in range(len(l)):
+        if i + 2 < len(l):
+            if l[i] == l[i+2]:
+                return False
+
+for i in range(1000, 1000000):
     broken_list = []
 
     while i != 0:
@@ -9,18 +24,33 @@ for i in range(1000):
 
     broken_list = broken_list[::-1]
 
-    for num in broken_list:
-        if num == 0:
-            a_list.append(num)
-        elif num == 1:
-            b_list.append(num)
-        elif num == 2:
-            c_list.append(num)
-        elif num == 3:
-            d_list.append(num)
-        elif num == 4:
-            e_list.append(num)
+    if sum(broken_list) == mult(broken_list) and check_valid(broken_list):
 
-    if len(broken_list) >= 3:
-        if broken_list[0] == len(b_list) and broken_list[2] == len(d_list):
-            print(i)
+        t1 = broken_list[0]
+
+        if broken_list[0] != broken_list[1]:
+            t2 = broken_list[1]
+        if broken_list[0] != broken_list[2] and broken_list[1] != broken_list[2]:
+            t3 = broken_list[2] 
+        if broken_list[0] != broken_list[3] and broken_list[1] != broken_list[3] and broken_list[2] != broken_list[3]:
+            t4 = broken_list[3]
+
+        for num in broken_list:
+            if num == t1:
+                t1_list.append(t1)
+            if num == t2:
+                t2_list.append(t2)
+            if num == t3:
+                t3_list.append(t3)
+            if num == t4:
+                t4_list.append(t4)
+
+        if t1 == len(t2_list):
+            print("")
+            print(f"{t1} {t2} {t3} {t4}")
+            print(f"{t1_list} {t2_list} {t3_list} {t4_list}")
+            print(f"Number : {broken_list}")
+            print("")
+        
+        t1, t2, t3, t4 = None, None, None, None
+        t1_list, t2_list, t3_list, t4_list = [], [], [], []
