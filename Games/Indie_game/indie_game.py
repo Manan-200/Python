3104adf6b8e6495a91ea8c357962c2c073c5adc5
Manan-200 <manandeopurkar@gmail.com>
2022-03-09 19:15:06 +0530

    Added player_img

--- Games/Indie_game/indie_game.py
+++ Games/Indie_game/indie_game.py
@@ -16,9 +16,10 @@ clock = pygame.time.Clock()
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Indie Game")
 
+#Player image
+player_img = pygame.transform.scale(pygame.image.load(os.path.join("assets", "player_img.png")) , (25 * 1.5, 136/4 * 1.5))
 #Background image
 bg_img = pygame.transform.scale(pygame.image.load(os.path.join("assets", "bg_img.png")), (width + 2*(-bg_x), height + 2*(-bg_y)))
-
 #Rock images
 rock_img_1 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "rock_imgs", "rock_img_1.png")), (rock_w, rock_h))
 rock_img_2 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "rock_imgs", "rock_img_2.png")), (rock_w, rock_h))
@@ -30,11 +31,11 @@ rock_img_7 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "ro
 #Tree images
 tree_img_1 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "tree_imgs", "tree_img_1.png")), (tree_w, tree_h))
 tree_img_2 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "tree_imgs", "tree_img_2.png")), (tree_w, tree_h))
-tree_img_3 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "tree_imgs", "tree_img_3.png")), (tree_w, tree_h))
-#tree_img_4 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "tree_imgs", "tree_img_4.png")), (tree_w, tree_h))
+tree_img_3 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "tree_imgs", "tree_img_3.png")), (59 * 2, 75 * 2))
+tree_img_4 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "tree_imgs", "tree_img_4.png")), (45 * 2, 69 * 2))
 #tree_img_5 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "tree_imgs", "tree_img_5.png")), (tree_w, tree_h))
 #tree_img_6 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "tree_imgs", "tree_img_6.png")), (tree_w, tree_h))
-#Removing background of rocks
+#Removing background of rocks, trees and player
 rock_img_1.set_colorkey((255, 255, 255))
 rock_img_2.set_colorkey((246, 246, 246))
 rock_img_3.set_colorkey((255, 255, 255))
@@ -42,13 +43,15 @@ rock_img_4.set_colorkey((220, 240, 244))
 rock_img_5.set_colorkey((220, 240, 244))
 rock_img_6.set_colorkey((220, 240, 244))
 rock_img_7.set_colorkey((220, 240, 244))
-#Removing background of trees
+
 tree_img_1.set_colorkey((255, 255, 255))
 tree_img_2.set_colorkey((255, 255, 255))
-tree_img_3.set_colorkey((0, 0, 0))
-#tree_img_4.set_colorkey((255, 255, 255))
+tree_img_3.set_colorkey((255, 255, 255))
+tree_img_4.set_colorkey((255, 255, 255))
 #tree_img_5.set_colorkey((255, 255, 255))
 #tree_img_6.set_colorkey((255, 255, 255))
+
+player_img.set_colorkey((255, 255, 255))
 
 #Player object
 class Player:
@@ -57,9 +60,11 @@ class Player:
         self.y = y
         self.w = w
         self.h = h
+        self.img = player_img
     def draw(self):
-        self.Rect = pygame.Rect(self.x - self.w/2, self.y - self.h/2, self.w, self.h)
-        pygame.draw.rect(win, (255, 0, 0), self.Rect)
+        """self.Rect = pygame.Rect(self.x - self.w/2, self.y - self.h/2, self.w, self.h)
+        pygame.draw.rect(win, (255, 0, 0), self.Rect)"""
+        win.blit(self.img, (self.x, self.y))
 
 #Rock object
 class Rock:
@@ -74,7 +79,7 @@ class Tree:
     def __init__(self):
         self.x = random.randrange(1, bg_img.get_width() - 50)
         self.y = random.randrange(1, bg_img.get_height() - 50)
-        self.img = random.choice([tree_img_1, tree_img_2])
+        self.img = random.choice([tree_img_1, tree_img_2, tree_img_3, tree_img_4])
     def draw(self):
         win.blit(self.img, (bg_x + self.x, bg_y + self.y))
 
