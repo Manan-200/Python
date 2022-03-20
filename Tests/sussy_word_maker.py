2ec705bbab292b3c6094a97a277f223bc8196047
Manan-200 <manandeopurkar@gmail.com>
2022-03-20 14:15:26 +0530

    SUS

---
+++ Tests/sussy_word_maker.py
@@ -0,0 +1,9 @@
+import random
+
+final_word = ""
+
+words = ["SUS", "SUSSY", "BAKA", "AMOGUS", "MILK", "BEAN", "MONKE", "THICC"]
+random.shuffle(words)
+for word in words:
+    final_word += word
+print(" \n The sussy word is: " + final_word, "\n")
