f45c0b31fa77fc1c7cdbc6dfc9f08d6a0d3a30da
Manan-200 <manandeopurkar@gmail.com>
2022-03-27 17:16:32 +0530

    checking defined

--- Tests/self_explanatory_num_1.py
+++ Tests/self_explanatory_num_1.py
@@ -1,5 +1,7 @@
 #14233221 Orjj 🙏🙏🙏
 
+counter = 0
+
 #Adding digits of number into an array
 def break_num(num):
     arr = []
@@ -8,14 +10,32 @@ def break_num(num):
         arr.append(r)
         num = num // 10
     arr = arr[::-1]
-    return arr
+    return(arr)
+
+#Function to remove duplicates
+def rem_dup(arr):
+    arr_new = arr
+    for index_1 in range(0, len(arr_new)):
+        for index_2 in range(0, len(arr_new)):
+            if index_1 != index_2 and arr_new[index_1] == arr_new[index_2]:
+                arr_new.pop(index_1)
+    return(arr_new)
 
 #Checking if in abcd, b == d(redefining)
-def check_redefine(arr):
+def check_redef(arr):
     for index_1 in range(1, len(arr), 2):
         for index_2 in range(1, len(arr), 2):
             if index_1 != index_2 and arr[index_1] == arr[index_2]:
                 return(False)
+
+#Checking if each definer is defined
+def check_def(arr):
+    definers= []
+    defineds = []
+    for index_1 in range(0, len(arr), 2):
+        definers.append(arr[index_1])
+    for index_2 in range(1, len(arr), 2):
+        defineds.append(arr[index_2])
 
 """
 The most important filter
@@ -33,10 +53,10 @@ def main_filter(arr):
 
 print("Self Explanatory Numbers are:")
 
-for num in range(10, 14233223):
+for num in range(10, 3000):
 
     #Defining conditions
-    c1, c2, c_main = True, True, True
+    c1, c2, c3, c_main = True, True, True, True
 
     broken_list = break_num(num)
     length = len(broken_list)
@@ -44,11 +64,14 @@ for num in range(10, 14233223):
     #Checking for condtions
     if length % 2 != 0:
         c1 = False
-    if check_redefine(broken_list) == False:
+    if check_redef(broken_list) == False:
         c2 = False
+    if check_def(broken_list) == False:
+        c3 = False
     if c1 == True and main_filter(broken_list) == False:
         c_main = False
     
     #Printing number if all conditions are true
-    if c1 == True and c2 == True and c_main == True:
-        print(num)
+    if c1 == True and c2 == True and c3 == True and c_main == True:
+        counter += 1
+        print(num, "counter = ", counter)
