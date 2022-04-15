7d6b77ff2c7755446f7412c937eab3cab5f3d01b
Manan-200 <manandeopurkar@gmail.com>
2022-04-15 15:50:15 +0530

    Completed

--- Random_programs/path_finder.py
+++ Random_programs/path_finder.py
@@ -1,16 +1,14 @@
-from re import X
 import pygame
 import random
 
 pygame.font.init()
 
 #Variables
-nodes, c_arr, node_arr, dist_list, node_f = [], [], [], [], []
+nodes, comb_arr, node_arr, dist_list = [], [], [], []
 running = True
 height, width = 600, 800
 dist = 0
-min_display_dist, max_display_dist = 0, 0
-fps = 15
+fps = 120
 main_counter, index_counter, draw_counter = 0, 0, 0
 
 #Clock
@@ -38,21 +36,21 @@ class Node:
         pygame.draw.rect(win, (0, 0, 255), (self.x - 5, self.y - 5, 10, 10))
 
 #Creating nodes
-
 for i in range (5):
     node = Node(i+1)
     nodes.append(node)
-node_0 = Node(0)
-#Making all combination of nodes
+node_0 = Node(0) #Initial node
+
+#Making all combination of numbers - 1 to 5
 for a in range (1,6):
     for b in range (1,6):
         for c in range (1,6):
             for d in range (1,6):
                 for e in range (1,6):
                     if a + b + c + d + e == 15 and a*b*c*d*e == 120:
-                        c_arr.append([a, b, c, d, e])
+                        comb_arr.append([a, b, c, d, e])
 
-#Function for calculation distance between two nodes
+
 def get_distance(node_1, node_2):
     distance = ((node_1.x - node_2.x)**2 + (node_1.y - node_2.y)**2) ** (1/2)
     return(distance)
@@ -67,7 +65,8 @@ def get_smallest(arr):
                     min_index = i
         return([min_index, min_val])
 
-for id_arr in c_arr:
+#Arranging Nodes based on number combinations in node_arr
+for id_arr in comb_arr:
     new_arr = []
     for id in id_arr:
         for node in nodes:
@@ -82,35 +81,66 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        #Resetting everything if space is pressed
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+
+                dist_list, node_arr, nodes = [], [], []
+                main_counter, index_counter, draw_counter = 0, 0, 0
+                dist = 0
+
+                for i in range (5):
+                    node = Node(i+1)
+                    nodes.append(node)
+                node_0 = Node(0)
+
+                for id_arr in comb_arr:
+                    new_arr = []
+                    for id in id_arr:
+                        for node in nodes:
+                            if node.id == id:
+                                new_arr.append(node)
+                    node_arr.append(new_arr)
+
+    win.fill((0, 0, 0))
 
     index_counter += 1
     main_counter += 1
-    win.fill((0, 0, 0))
+    
     dist = 0
 
+    #Reseting index counter if it exceeds the len(node_arr)
     if index_counter >= len(node_arr):
         index_counter = 0
 
+    #Specifying nodes
     nodes = node_arr[index_counter]
 
+    #Drawing all combinations of Nodes once
     if draw_counter <= len(node_arr):
+        #Connecting nodes with lines and calculating distance
         for p in range(len(nodes) - 1):
             pygame.draw.line(win, (150, 150, 150), (nodes[p].x, nodes[p].y), (nodes[p + 1].x, nodes[p + 1].y))
             dist += get_distance(nodes[p], nodes[p + 1])
-            draw_counter += 1
+        #Connecting initial node with the first node and adding distance
         dist += get_distance(nodes[0], node_0)
         pygame.draw.line(win, (150, 150, 150), (nodes[0].x, nodes[0].y), (node_0.x, node_0.y))
         dist_list.append(dist)
+        draw_counter += 1
     
+    #Connecting nodes with minimum distance
     if draw_counter > len(node_arr):
         min_index, min_dist = get_smallest(dist_list)
-        nodes = node_arr[min_index]
+        nodes = node_arr[min_index + 1]
+
         for p in range(len(nodes) - 1):
             pygame.draw.line(win, (150, 150, 150), (nodes[p].x, nodes[p].y), (nodes[p + 1].x, nodes[p + 1].y))
         pygame.draw.line(win, (150, 150, 150), (nodes[0].x, nodes[0].y), (node_0.x, node_0.y))
+
         dist_text = dist_font.render((f"distance: {round(min_dist)} pixels"), 1, (255, 255, 255))
         win.blit(dist_text, (0, 0))
 
+    #Drawing nodes, blitting distance and id
     for node in nodes:
         id_text = id_font.render(str(node.id), 1, (255, 0, 0))
         node.draw()
@@ -120,3 +150,26 @@ while running:
     win.blit(id_text, (node_0.x - (id_text.get_width()/2), node_0.y - (id_text.get_height()/2) - 10))
 
     pygame.display.update()
+
+
+if (int(input("Check? (1/0)")) == 1):
+    test_dist = 0
+    test_nodes = []
+    id_list = []
+
+    for i in range(5):
+        id = int(input(f"Enter node id: "))
+        id_list.append(id)
+
+    for id in id_list:
+        for i in range(len(nodes)):
+            if nodes[i].id == id:
+                test_nodes.append(nodes[i])
+
+    for i in range(len(test_nodes) - 1):
+        test_dist += get_distance(test_nodes[i], test_nodes[i + 1])
+    test_dist += get_distance(test_nodes[0], node_0)
+
+    print(test_dist, min_dist)
+    print(len(dist_list), len(node_arr))
+    print("\n\n\n", dist_list)
