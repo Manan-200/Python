061ccbd47a1080ab5011603edbd4ed739d745c81
Manan-200 <manandeopurkar@gmail.com>
2022-04-21 20:03:01 +0530

    added attacking function

--- Games/Stratedy_game/strategy_game_with_pygame.py
+++ Games/Stratedy_game/strategy_game_with_pygame.py
@@ -1,17 +1,19 @@
 import pygame
+import random
 
 pygame.font.init()
 player_font = pygame.font.SysFont("Calibri", 20)
 bot_font = pygame.font.SysFont("Calibri", 15)
 
 time_survived = 0
-available_territory = 10000
+tot_terr = 10000
 width, height = 1000, 700
 fps = 100
 counter = 0
 clock = pygame.time.Clock()
 key_cooldown = 0
 bots = []
+entities = []
 
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("strategy game")
@@ -21,7 +23,7 @@ class Player:
     territory_flag = False
 
     def __init__(self, ID):
-        self.id = ID
+        self.ID = ID
         self.army = 0
         self.territory = 1000
         self.interest = None           
@@ -34,18 +36,21 @@ class Player:
         self.player_stats = player_font.render(f"Your stats :- Army : {self.army}; Territory  : {self.territory}; Interest : {self.interest}; Time survived : {round(time_survived)}", 1, (255, 255, 255))
         win.blit(self.player_stats, (width/2 - self.player_stats.get_width()/2, height/2 - self.player_stats.get_height()/2))
 
-    def expand(self, percent):
-        global available_territory
-        army_per = round(self.army*percent/100)
-        while available_territory - army_per < 0:
+    def expand(self, per):
+        army_per = round(self.army*per/100)
+        while available_terr - army_per < 0:
             army_per -= 1
         self.territory += army_per
         self.army -= army_per
-        #bot_1.army -= 10
-        available_territory -= army_per
 
+    def attack(self, enemy, per):
+        army_per = round(self.army*per/100)
+        while enemy.territory - army_per/2 < 0:
+            army_per -= 1
+        enemy.territory -= army_per/2
+        self.army -= army_per
 
-    def draw_percent_bar(self):
+    def draw_per_bar(self):
         redbarRect = pygame.Rect(840, 300, 20, 100)
         bluebarRect = pygame.Rect(840, self.bluebar_y_val, 20, 5)
         if pygame.mouse.get_pos()[0] >= 840 and pygame.mouse.get_pos()[0] <= 860 and pygame.mouse.get_pos()[1] >= 300 and pygame.mouse.get_pos()[1] <= 400 and pygame.mouse.get_pressed(num_buttons = 3):
@@ -56,16 +61,44 @@ class Player:
 
 class Bot(Player):
     def draw(self):
-        self.bot_stats = bot_font.render(f"Bot {self.id}'s stats :- Army : {self.army}; Territory  : {self.territory}; Interest : {self.interest}; Time survived : {round(time_survived)}", 1, (255, 255, 255))
-        win.blit(self.bot_stats, (width/2 - self.bot_stats.get_width()/2, height/2 + player.player_stats.get_height()/2 + self.id*50))
+        self.bot_stats = bot_font.render(f"Bot {self.ID}'s stats :- Army : {self.army}; Territory  : {self.territory}; Interest : {self.interest}; Time survived : {round(time_survived)}", 1, (255, 255, 255))
+        win.blit(self.bot_stats, (width/2 - self.bot_stats.get_width()/2, height/2 + player.player_stats.get_height()/2 + self.ID*50))
 
-            
+    def attack(self):
+        #Randomly selecting an entity to attack
+        enemy_ID = self.ID
+        while enemy_ID != self.ID:
+            enemy_ID = random.randint(0, len(entities) + 1)
+        for entity in entities:
+            if entity.ID == enemy_ID:
+                enemy = entity
+        #Calculating the percentage of army to attack
+        if self.army < enemy.army:
+            per = random.randint(1, 21)
+        elif self.army > enemy.army:
+            per = random.randint(21, 71)
+        elif self.army == enemy.army:
+            per = random.randint(10, 31)
+        #Attacking
+        army_per = round(self.army*per/100)
+        while enemy.territory - army_per/2 < 0:
+            army_per -= 1
+        enemy.territory -= army_per/2
+        self.army -= army_per
+
+
+
 player = Player(0)
+entities.append(player)
 for i in range(1, 6):
     bot = Bot(i)
     bots.append(bot)
+    entities.append(bot)
 
-while True:
+
+
+running = True
+while running:
 
     clock.tick(fps)
 
@@ -74,23 +107,33 @@ while True:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            quit()
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_e:
+                player.expand(50)
+            if event.key == pygame.K_a:
+                player.attack(random.choice(bots), 50)
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_e] and key_cooldown <= var:
-        key_cooldown += 1
-        player.expand(50)
-
-    if var % 1 == 0:  #is similar to time.delay(1) so that the whole program doesn't have to wait 1 second 
+    if var % 1 == 0:  #is similar to time.delay(1) so that the whole program doesn't have to wait 1 second
         time_survived += 1
         player.handle_interest()
         for bot in bots:
-            bot.handle_interest()
+            if bot.territory == 0:
+                bots.remove(bot)
+            else:
+                bot.handle_interest()
+                choice = random.randint(1, 2)
+                if choice == 1:
+                    bot.attack()
+    
+    bots_terr = 0
+    for bot in bots:
+        bots_terr += bot.territory
+    available_terr = tot_terr - (player.territory + bots_terr)
 
     win.fill((0, 0, 0))
     player.draw()
     for bot in bots:
         bot.draw()
-    #player.draw_percent_bar()
 
     pygame.display.update()
