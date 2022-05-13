b91f8df1bb5b2b3482f5b72b5babd69bb06f9eb7
Manan-200 <manandeopurkar@gmail.com>
2022-05-13 07:58:46 +0530

    improved code

--- Tests/reversing_array.py
+++ Tests/reversing_array.py
@@ -1,15 +1,9 @@
-array = [1, "+", 2, "-", 3, "*", 4, "/", 5]
+arr = [1, "+", 2, "-", 3, "*", 4, "/", 5]
+print(f"original array: {arr}")
+i, j = 0, len(arr) - 1
 
-class ReverseArray:
-    def __init__(self, array):
-        self.array = array
-        self.reveresed_array = []
-    def reverse(self):
-        for i in range(len(self.array)):
-            num = (i + 1) * -1
-            self.reveresed_array.append(self.array[num])
-        print(self.array)
-        print(self.reveresed_array)
-    
-array = ReverseArray(array)
-array.reverse()
+while i < j:
+    arr[i], arr[j] = arr[j], arr[i]
+    i += 1
+    j -= 1
+print(f"reversed array: {arr}")
