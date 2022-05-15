264c8125f85464bc1547570978c17577be2df605
Manan-200 <manandeopurkar@gmail.com>
2022-05-15 10:06:58 +0530

    Added deleting of keys

--- Apps/Multi_Clipboard/multi_clip.py
+++ Apps/Multi_Clipboard/multi_clip.py
@@ -5,18 +5,19 @@ import json
 SAVED_DATA = "clipboard.json"
 
 def save_data(filepath, data):
-    with open(filepath, "w") as f:
-        json.dump(data, f)
+    with open(filepath, "w") as file:
+        json.dump(data, file)
 
 def load_data(filepath):
     try:
-        with open(filepath, "r") as f:
-            data = json.load(f)
+        with open(filepath, "r") as file:
+            data = json.load(file)
             return(data)
     except:
         return {}
 
 if len(sys.argv) == 2:
+
     cmd = sys.argv[1]
     data = load_data(SAVED_DATA)
 
@@ -31,14 +32,23 @@ if len(sys.argv) == 2:
             clipboard.copy(data[key])
             print(f"Copied '{data[key]}' to clipboard.")
         else:
-            print("Key doesn't exist.")
+            print(f"{key} doesn't exist in data.")
     elif cmd == "list":
         print(data)
     elif cmd == "clear":
         data = {}
         save_data(SAVED_DATA, data)
         print("Data cleared.")
+    elif cmd == "del":
+        key = input("Enter a key: ")
+        if key in data:
+            print(f"Deleted '{data[key]}' at {key}.")
+            del data[key]
+            save_data(SAVED_DATA, data)
+        else:
+            print(f"{key} doesn't exist in data.")
     else:
         print("Unknown command.")
+
 else:
     print("Please pass exactly one command.")
