0766a7d5a23422de38359c473376046973c8e8fd
Manan-200 <manandeopurkar@gmail.com>
2022-05-24 13:13:22 +0530

    replaced and added combination generator

---
+++ Random_programs/Path_finder/combination_gen.py
@@ -0,0 +1,45 @@
+num = 6
+NUM = num
+counter = 0
+
+def init_num(n):
+    if n == 0:
+        return(0)
+    else:
+        return(n*10**(NUM-n)) + init_num(n - 1)
+
+def final_num(n):
+    if n == 0:
+        return(0)
+    else:
+        return((n * 10**(n-1)) + final_num(n-1))
+
+def break_num(n):
+    rem_arr = []
+    while n != 0:
+        rem = n % 10
+        n = n // 10
+        rem_arr.append(rem)
+    return(rem_arr[::-1])
+
+def mult_num(arr):
+    mult = 1
+    for i in arr:
+        mult *= i
+    return(mult)
+    
+def sum_num(arr):
+    Sum = 0
+    for i in arr:
+        Sum += i
+    return(Sum)
+
+f_num = final_num(num)
+i_num = init_num(num)
+
+for n in range(i_num, f_num + 1):
+    num_arr = break_num(n)
+
+    if mult_num(break_num(f_num)) == mult_num(num_arr) and sum_num(break_num(f_num)) == sum_num(num_arr) and len(num_arr) == num:
+        counter += 1
+        print(num_arr, counter)
