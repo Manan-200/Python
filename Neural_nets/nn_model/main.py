3109e26f773e4f7b589a5901ca19b10fe70d10c8
Manan-200 <manandeopurkar@gmail.com>
2022-08-11 18:55:24 +0530

    making neural network from scratch

---
+++ Neural_nets/nn_model/main.py
@@ -0,0 +1,25 @@
+import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+
+nnfs.init()
+
+X, y = spiral_data(100, 3)
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+layer1 = Layer_Dense(2, 5)
+activation1 = Activation_ReLU()
+
+layer1.forward(X)
+activation1.forward(layer1.output)
+print(activation1.output)
