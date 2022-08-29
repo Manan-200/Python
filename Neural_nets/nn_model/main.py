69889053b931240204a81449f0e7771ea10eca7e
Manan-200 <manandeopurkar@gmail.com>
2022-08-29 23:54:47 +0530

    changes

--- Neural_nets/nn_model/main.py
+++ Neural_nets/nn_model/main.py
@@ -18,12 +18,21 @@ class Activation_ReLU:
 class Activation_Softmax:
     def forward(self, inputs):
         exp_values = np.exp(inputs - np.max(inputs, axis = 1, keepdims = True))
+        probabilities = exp_values/np.sum(exp_values, axis = 1, keepdims = True)
+        self.output = probabilities
 
-layer1 = Layer_Dense(2, 5)
+X, y = spiral_data(samples = 100, classes = 3)
+
+dense1 = Layer_Dense(2, 3)
 activation1 = Activation_ReLU()
 
-layer1.forward(X)
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
 
+dense1.forward(X)
+activation1.forward(dense1.output)
 
-activation1.forward(layer1.output)
-print(activation1.output)
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
