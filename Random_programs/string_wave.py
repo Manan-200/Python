a50db1b340554a3acb57c0c922023b012564f220
Manan-200 <manandeopurkar@gmail.com>
2022-09-04 23:49:44 +0530

    wavey

--- Random_programs/string_wave.py
+++ Random_programs/string_wave.py
@@ -1,15 +1,16 @@
 string = input("Enter string: ")
 waves = int(input("Enter number of waves: "))
 wavelength = 10
-spaces = 0
-for w in range(waves):
-    for i in range(wavelength):
+counter = 0
+
+def calc_spaces(wl, counter):
+    pos = counter%(wl*2)
+    spaces = wl - abs(wl - pos)
+    return(spaces)
+
+for i in range(waves):
+    for counter in range(wavelength*2):
+        spaces = calc_spaces(wavelength, counter)
         for _ in range(spaces):
             print(" ", end="")
         print(string)
-        spaces += 1
-    for i in range(wavelength - 1):
-        for _ in range(spaces):
-            print(" ", end = "")
-        print(string)
-        spaces -= 1
