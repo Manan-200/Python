82551fadcf3b031d38fdbda705f15091807c6ebf
Manan-200 <manandeopurkar@gmail.com>
2022-11-04 19:53:28 +0530

    ok

---
+++ Apps/Chatroom/server.py
@@ -0,0 +1,40 @@
+import socket
+import threading
+
+HEADER = 64
+PORT = 5065
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "!disconnect"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected. ")
+
+    connected = True
+    while connected:
+        msg_len = conn.recv(HEADER).decode(FORMAT)
+        if msg_len:
+            msg_len = int(msg_len)
+            msg = conn.recv(msg_len).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+
+            print(f"[{addr}] {msg}")
+            conn.send("Msg recieved".encode(FORMAT))
+    conn.close()
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target = handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTION] {threading.active_count() - 1}")
+
+print("[STARTING] server is starting... ")
+start()
