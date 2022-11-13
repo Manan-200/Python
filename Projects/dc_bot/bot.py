1f869c4504792aaf19a42f9fc5a94af4906ea3d1
Manan-200 <manandeopurkar@gmail.com>
2022-11-13 20:37:09 +0530

    removed ayush's shitty code

--- Projects/dc_bot/bot.py
+++ Projects/dc_bot/bot.py
@@ -26,10 +26,5 @@ async def hello(interaction: discord.Interaction):
 @app_commands.describe(msg = "what should I say?")
 async def  say(interaction: discord.Interaction, msg:str):
     await interaction.response.send_message(f"{msg}")
-
-@bot.tree.command(name="lmao)
-async def lmao(interaction: discord.Interaction):
-    await interaction.response.send_message(f"manan's life is funny yes!)
-                                            
                                             
 bot.run(TOKEN)
