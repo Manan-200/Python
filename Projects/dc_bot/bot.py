6103633a2ec307dc02fe9a576b49b3ba5418b258
Manan-200 <manandeopurkar@gmail.com>
2022-11-13 22:01:03 +0530

    added embedding

--- Projects/dc_bot/bot.py
+++ Projects/dc_bot/bot.py
@@ -34,5 +34,21 @@ async def games(interaction: discord.Interaction):
 @bot.tree.command(name="ping")
 async def ping(interaction: discord.Interaction):
     await interaction.response.send_message("pong!")
+
+@bot.tree.command(name="embed")
+async def embed(interaction: discord.Interaction, member:discord.Member = None):
+    if member == None:
+        member = interaction.user
     
+    name = member.display_name
+    pfp = member.display_avatar
+
+    embed = discord.Embed(title=f"{name} you suck", description=f"{name} sucks", colour=discord.Colour.random())
+    embed.set_author (name=f'{name}')
+    embed.set_thumbnail(url=f"{pfp}")
+    embed.add_field(name=f"{name} is a disgrace!", value=f"{name} is fatherless!")
+    embed.set_footer(text=f"{interaction.user} created this embed!")
+
+    await interaction.response.send_message(embed=embed)
+
 bot.run(TOKEN)
